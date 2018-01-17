d75cbd38c67c9fc96214a556a8c16c863f58b018
douqing <qqdou@thoughtworks.com>
2018-01-17 20:03:00 +0800

    add CompareNumber test

--- Three-SiWo-HomeWork/spec/GuessNumber/guessNumberSpec.js
+++ Three-SiWo-HomeWork/spec/GuessNumber/guessNumberSpec.js
@@ -1,7 +1,11 @@
-let controller = require('../../GuessNumber/CompareNumber');
+let controller = require('../../GuessNumber/GuessNumber');
 describe("guessNumber function shouble be corrent", function () {
-    it(" guessNumber function", function () {
-        let result = controller.CompareNumber();
-        expect(result).toEqual('hello')
+    let randomData = "7486";
+    it("CompareNumber function should output XAXB", function () {
+        let input = "3456";
+        let answer = "2957";
+        let XAXB = "1A0B";
+        let result = controller.CompareNumber(input, answer);
+        expect(result).toEqual(XAXB);
     });
 });
