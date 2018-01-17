a0ee390e303469aff19532bbed65ea1223aa0015
douqing <qqdou@thoughtworks.com>
2018-01-17 20:08:34 +0800

    add  AnswerGenerator test

--- Three-SiWo-HomeWork/spec/GuessNumber/guessNumberSpec.js
+++ Three-SiWo-HomeWork/spec/GuessNumber/guessNumberSpec.js
@@ -1,11 +1,22 @@
 let controller = require('../../GuessNumber/GuessNumber');
 describe("guessNumber function shouble be corrent", function () {
-    let randomData = "7486";
     it("CompareNumber function should output XAXB", function () {
-        let input = "3456";
-        let answer = "2957";
-        let XAXB = "1A0B";
+        const input = "3456";
+        const answer = "2957";
+        const XAXB = "1A0B";
         let result = controller.CompareNumber(input, answer);
         expect(result).toEqual(XAXB);
     });
+    it("AnswerGenerator function should be right", function () {
+
+        expect(controller.AnswerGenerator().length).toEqual(4);
+
+        expect(controller.AnswerGenerator()).not.toEqual(controller.AnswerGenerator());
+
+        const answers = controller.AnswerGenerator().split('');
+        answers.forEach((answer) => {
+            expect(answers.indexOf(answer)).toEqual(answers.lastIndexOf(answer));
+        })
+
+    })
 });
